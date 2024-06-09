e1b8902adf075167d4bf7f6bf39a0e3abdfa5885
Robin <92713851+robinkutde@users.noreply.github.com>
2024-06-09 12:42:03 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+if (( $EUID != 0 )); then
+    echo "Please run as root"
+    exit
+fi
+
+clear
+
+installTheme(){
+    cd /var/www/
+    tar -cvf Floppa_Theme_Backup.tar.gz pterodactyl
+    echo "Installing theme..."
+    cd /var/www/pterodactyl
+    rm -r Floppa_Theme
+    git clone https://github.com/robinkutde/floppa-theme.git
+    cd Floppa_Theme
+    rm /var/www/pterodactyl/resources/scripts/Floppa-Theme.css
+    rm /var/www/pterodactyl/resources/scripts/index.tsx
+    mv index.tsx /var/www/pterodactyl/resources/scripts/index.tsx
+    mv Floppa-Theme.css /var/www/pterodactyl/resources/scripts/Floppa-Theme.css
+    cd /var/www/pterodactyl
+
+    curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+    apt update
+    apt install -y nodejs
+
+    npm i -g yarn
+    yarn
+
+    cd /var/www/pterodactyl
+    yarn build:production
+    sudo php artisan optimize:clear
+
+
+}
+
+installThemeQuestion(){
+    while true; do
+        read -p "Are you sure that you want to install the theme [y/n]? " yn
+        case $yn in
+            [Yy]* ) installTheme; break;;
+            [Nn]* ) exit;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+}
+
+repair(){
+    bash <(curl https://raw.githubusercontent.com/robinkutde/floppa-theme/main/repair.sh)
+}
+
+restoreBackUp(){
+    echo "Restoring backup..."
+    cd /var/www/
+    tar -xvf Floppa_Theme_Backup.tar.gz
+    rm Floppa_Theme_Backup.tar.gz
+
+    cd /var/www/pterodactyl
+    yarn build:production
+    sudo php artisan optimize:clear
+}
+echo "Copyright (c) 2024 Angelillo15 and NoPro200"
+echo "This program is free software: you can redistribute it and/or modify"
+echo ""
+echo ""
+echo "[1] Install theme"
+echo "[2] Restore backup"
+echo "[3] Repair panel (use if you have an error in the theme installation)"
+echo "[4] Exit"
+
+read -p "Please enter a number: " choice
+if [ $choice == "1" ]
+    then
+    installThemeQuestion
+fi
+if [ $choice == "2" ]
+    then
+    restoreBackUp
+fi
+if [ $choice == "3" ]
+    then
+    repair
+fi
+if [ $choice == "4" ]
+    then
+    exit
+fi
